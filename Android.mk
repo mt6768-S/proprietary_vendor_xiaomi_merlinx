2479e0be6b812a95ab350aba5a7711937b5ceafa
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-17 14:27:12 +0200

    merlinx: Initial vendor tree
    Change-Id: I4e194b2a56fde1b96336ae2e83fdb6fd06a40db6

---
+++ Android.mk
@@ -0,0 +1,17 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),merlinx)
+
+$(call add-radio-file-sha1-checked,radio/lk.img,0dd729e1a4f2f58691d0a644e0643f9e6061ad6f)
+$(call add-radio-file-sha1-checked,radio/md1img.img,a51476dafcf9d580dd2ebab240f47d87a1df4425)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1d3313c01ed643f786d5056cd04d6807d1142c42)
+$(call add-radio-file-sha1-checked,radio/scp.img,808133186818342c525027c9b57515015548148a)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b36e602c456959f08e127f31c06d10e516853645)
+$(call add-radio-file-sha1-checked,radio/sspm.img,643dfdff44ccc82cf6c45e1bb5c77c365cbe8863)
+$(call add-radio-file-sha1-checked,radio/tee.img,796b60d4a4e46f23973f4b5e9030e152b5329339)
+
+endif
